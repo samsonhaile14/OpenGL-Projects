cb9761190e3439e59ffcbe7d5177c9e300905b5e
samsonhaile28 <shaile@nevada.unr.edu>
2016-10-02 21:48:25 -0700

    Ported code from PA4 to PA5, removed manual object loader, included linkage of assimp library

---
+++ PA5/include/engine.h
@@ -0,0 +1,44 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+#include <sys/time.h>
+#include <assert.h>
+
+#include "window.h"
+#include "graphics.h"
+
+class Engine
+{
+  public:
+    Engine(string name, int width, int height, 
+           std::vector< std::string > fileNames );
+    Engine(string name);
+    ~Engine();
+    bool Initialize();
+    void Run();
+    void Keyboard();
+    unsigned int getDT();
+    long long GetCurrentTimeMillis();
+  
+  private:
+    // Window related variables
+    Window *m_window;    
+    string m_WINDOW_NAME;
+    int m_WINDOW_WIDTH;
+    int m_WINDOW_HEIGHT;
+    bool m_FULLSCREEN;
+    std::vector< std::string > gFiles;
+    SDL_Event m_event;
+
+    Graphics *m_graphics;
+
+    float movement[2] = {1.0f,1.0f}; //index 0 = rotation, index 1 = orbit
+    bool pause = false; 
+
+
+    unsigned int m_DT;
+    long long m_currentTimeMillis;
+    bool m_running;
+};
+
+#endif // ENGINE_H
